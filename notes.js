7de6f237bd4ac17455fd79b2c3cbb268a46a7ae0
allisonkosborne <allisonkosborne@me.com>
2021-10-31 20:14:12 -0500

    note ex practice

--- notes.js
+++ notes.js
@@ -1,6 +1,19 @@
 const notes = [
   {
     id: 1,
+    subject: "javascript",
+    date: 10 / 31 / 2021,
+    feeling: "confident",
+    timeSpent: "60",
   },
-  {},
+
+  {
+    id: 2,
+    subject: "gitHub",
+    date: 10 / 30 / 2021,
+    feeling: "confused",
+    timeSpent: "45",
+  },
 ];
+
+console.log();
