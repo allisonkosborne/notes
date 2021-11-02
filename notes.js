87c824ed7e8b143b18634ebb28698a181c9e6630
allisonkosborne <allisonkosborne@me.com>
2021-11-01 20:31:03 -0500

    more details

--- notes.js
+++ notes.js
@@ -15,3 +15,27 @@ const notes = [
     timeSpent: "45",
   },
 ];
+
+const noteAboutToday = {
+  id: 3,
+  subject: "push",
+  date: 11 / 01 / 2021,
+  feeling: "confident",
+  timeSpent: 30,
+};
+
+notes.push(noteAboutToday);
+
+console.log(notes);
+
+console.log(`Notes ${notes[0].id}`);
+console.log(notes[0].id);
+console.log(`I learned ${notes[0].subject}.`);
+console.log(`I spent ${notes[0].timeSpent} minutes on it.`);
+console.log(`I felt ${notes[0].feeling}.`);
+
+for (const scribblings of notes) {
+  console.log(`Notation Number ${scribblings.id} 
+  Subject is $[scribblings.subject]
+  Time spent is ${scribblings.timeSpent}`);
+}
